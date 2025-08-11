08b2cfca7831ca07160ad7b21ca80c77a1868bf2
Xuanqi He <xuanqihe@amazon.com>
2025-08-11 11:15:45 -0400

    Add list_attached_role_policies and detach_role_policy in iam.py

--- cli/src/pcluster/aws/iam.py
+++ cli/src/pcluster/aws/iam.py
@@ -57,3 +57,13 @@ class IamClient(Boto3Client):
     def tag_role(self, role_name, tags):
         """Add or overwrite one or more tags for the specified role."""
         return self._client.tag_role(RoleName=role_name, Tags=tags)
+
+    @AWSExceptionHandler.handle_client_exception
+    def list_attached_role_policies(self, role_name):
+        """Add or overwrite one or more tags for the specified role."""
+        return self._client.list_attached_role_policies(RoleName=role_name)
+
+    @AWSExceptionHandler.handle_client_exception
+    def detach_role_policy(self, role_name, policy_arn):
+        """Add or overwrite one or more tags for the specified role."""
+        return self._client.detach_role_policy(RoleName=role_name, PolicyArn=policy_arn)
